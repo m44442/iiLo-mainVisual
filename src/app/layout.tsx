8aa40ee65e6c98843e7a47ba7f7992bd1fc9f000
m44442 <rikumatsumoto.2003@gmail.com>
2025-07-03 11:51:41 +0900

    書体

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import "./globals.css";
 import CustomCursor from "./components/CustomCursor";
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Three.js Particle Noise",
@@ -14,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <CustomCursor />
         {children}
       </body>
